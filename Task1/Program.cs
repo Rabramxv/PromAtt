d15a33f3a74028eba4e792433cc63fef7b1888d1
Roman Krotov <krotan152@gmail.com>
2024-03-19 22:14:13 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumbers(int n, int m)
+{
+    if(n>m) return;
+    System.Console.Write(n + " "); 
+    PrintNumbers(n+1,m);
+}
+
+int N = ReadInt("Введите число: ");
+int M = ReadInt("Введите число: ");
+PrintNumbers(N,M);
